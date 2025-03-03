1a5d959dcd96739c7a7280922d0f73f1f3e68aea
chrism1215 <cmcd1144@students.vsu.edu>
2025-03-03 15:26:18 -0500

    Update Node.Test.cs

--- QuadtreeLibrary.Tests/Node.Test.cs
+++ QuadtreeLibrary.Tests/Node.Test.cs
@@ -1,1 +1,6 @@
+namespace Quadtree.Tests;
+
+// NOTE: This file should be removed. Abstract classes cannot be instantiated directly.
+// Instead, concrete implementations of abstract classes should be tested.
+// In this case, rather than attempting to test 'Node', focus on testing 'LeafNode' and 'InternalNode'.
 
